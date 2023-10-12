ce482c57bfb0c508cbb0e0b88b94c55693e920bd
beliberda <anton.nurgaliev@mail.ru>
2023-10-12 15:31:10 +1000

    add components styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import buttonEnter from "Components/UI/buttons";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Заголовок 1</h1>
+      <h2>Заголовок 2</h2>
+      <h3>Заголовок 3</h3>
+      <h4>Заголовок 4</h4>
+      <h5>Заголовок 5</h5>
+      <p>Текст рыба обычный текст</p>
+      <buttonEnter></buttonEnter>
+      <input type="text" />
+      <select name="select" id="select_id">
+        <option value="" key="" selected>
+          опция 1
+        </option>
+        <option value="" key="">
+          опция 2
+        </option>
+        <option value="" key="">
+          опция 3
+        </option>
+      </select>
+    </>
   );
 }
 
---
+++ src/Components/UI/buttons/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export default function buttonEnter() {
+  return <button className="button-enter">Войти</button>;
+}
+const buttonQr = () => {
+  return <button className="button-enter"></button>;
+};
+const buttonDisableQr = () => {
+  return <button className="button-enter"></button>;
+};
