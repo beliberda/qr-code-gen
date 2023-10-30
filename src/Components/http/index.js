5d5873501451ee955c2a0ee6dc1f92e244aa223c
beliberda <anton.nurgaliev@mail.ru>
2023-10-30 18:04:23 +1000

    change link

--- src/Components/http/index.js
+++ src/Components/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const API_URL = " http://127.0.0.1:8225/";
-export const API_URL = "http://86.102.19.21:8225/";
+export const API_URL = "https://solweb.site/";
 // export const API_URL = "http://192.168.31.218/";
 
 const $api = axios.create({
